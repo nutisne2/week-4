6378f093ed1e2a205d87c3e253284dc7b2d1f56b
unknown-user <unknown-user@10.70.45.82>
2015-09-11 10:42:48 +0700

    initializing

---
+++ week4/Lab402/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Shape s1 = new Rectangle("red", 4, 5);
+			Console.WriteLine (s1);
+			Console.WriteLine ("Area is " + s1.getArea());
+
+			Shape s2 = new Triangle("blue", 4, 5);
+			Console.WriteLine (s2);
+			Console.WriteLine ("Area is " + s2.getArea());
+
+			// Cannot create instance of an abstract class - Compilation Error!!
+			//      Shape s3 = new Shape("green");
+		}
+	}
+}
