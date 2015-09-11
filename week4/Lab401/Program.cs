aee622c19e96780eb4ff89b8d5efce0336276781
unknown-user <unknown-user@10.70.45.82>
2015-09-11 15:11:52 +0700

    add main program

---
+++ week4/Lab401/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Test Program for Lab401
+			Circle c1=new Circle(), c2=new Circle(1.5,5.0,2), c3=new Circle(c2);
+			Console.WriteLine(c1+"\n"+c2+"\n"+c3);
+
+			Cylinder cl1 = new Cylinder (), cl2 = new Cylinder (c3), cl3 = new Cylinder (1, 1, 3, 4);
+			Cylinder cl4 = new Cylinder (cl3);
+			Console.WriteLine(cl1+"\n"+cl2+"\n"+cl3+"\n"+cl4);
+		}
+	}
+}
